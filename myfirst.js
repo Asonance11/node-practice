13a96bbcf984bbfd6714cbf60ed42d597c275ba2
Oluwadabira <adigunsamuelaso@gmail.com>
2023-08-06 17:42:44 +0100

    Importing first module

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var dt = require('./firstModule');
 
 http
 	.createServer((req, res) => {
 		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.end('Hello World');
+		res.end(`The date and time is currently ${dt.myDateTime()}`);
 	})
 	.listen(8080);
